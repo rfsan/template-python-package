bd44cce6f9d0f1d2e37d6377a481bdda6b4f72ff
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2023-06-14 06:48:57 -0500

    First commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+env = os.environ.copy()
+
+# Choose Python version
+env["PYENV_VERSION"] = "{{ cookiecutter.python_dev_version }}"
+
+# Create virtual environment
+subprocess.run(
+    [
+        "python",
+        "-m",
+        "venv",
+        ".venv",
+        "--prompt",
+        "{{ cookiecutter.package_distribution_name }}",
+    ],
+    env=env,
+)
+
+# Install the package in editable mode
+subprocess.run(
+    [
+        ".venv/bin/pip",
+        "install",
+        "-e",
+        ".",
+    ],
+    env=env,
+)
+
+# git init
+subprocess.run(["git", "init"])
+subprocess.run(['git', 'branch', '-M', 'main'])
