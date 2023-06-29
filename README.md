b099179b736eebb30ee2d638965397824ad1e931
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2023-06-29 16:30:23 -0500

    Add quiet mode for the hooks and MIT as the License

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-- VS Code
+# Python Package Template
+
+## Requirements
+
+- pyenv
+
+## Details
+
+- MIT License
+- A virtual environment `.venv` is created in the root of the project
+- The package is installed in editable mode
+- A git repo is initialized with a `main` branch
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -23,12 +23,21 @@ subprocess.run(
 subprocess.run(
     [
         ".venv/bin/pip",
+        "-q",  # quiet mode
         "install",
-        "-e",
+        "-e",  # editable
         ".",
     ],
     env=env,
 )
 
-# git init
-subprocess.run(["git", "init", "-b", "main"])
+# git init with an initial branch named `main` and in quiet mode
+subprocess.run(
+    [
+        "git",
+        "init",
+        "-q",  # quiet mode
+        "-b",  # name of the initial branch
+        "main",
+    ]
+)
